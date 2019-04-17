b64e5bff087b4576872d707fb1c328e2620306b7
Andrey <Ihdota2@mail.ru>
2019-04-17 19:11:13 +0300

    bag fixed

--- js/new-window.js
+++ js/new-window.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
   $(".mymagicoverbox").click(function() {
-    $('#myfond_gris').fadeIn(300);
+    $('#blackout').fadeIn(300);
     var iddiv = $(this).attr("iddiv");
     $('#' + iddiv).fadeIn(300);
-    $('#myfond_gris').attr('opendiv', iddiv);
+    $('#blackout').attr('opendiv', iddiv);
     return false;
   });
 
-  $('#myfond_gris, .mymagicoverbox_fermer').click(function() {
-    var iddiv = $("#myfond_gris").attr('opendiv');
-    $('#myfond_gris').fadeOut(300);
+  $('#blackout, .mymagicoverbox_fermer').click(function() {
+    var iddiv = $("#blackout").attr('opendiv');
+    $('#blackout').fadeOut(300);
     $('#' + iddiv).fadeOut(300);
   });
 
